c19253b4086075346508ef2f181808d3b1f6511c
jef_b <jef_b@DESKTOP-44DK2L7>
2017-03-08 22:12:32 +0100

    Attributen Kosten en Baten toegevoegd, klassen moeten nog gemaakt worden

--- src/Projecten2/Models/Domain/Kosten.cs
+++ src/Projecten2/Models/Domain/Kosten.cs
@@ -11,5 +11,13 @@ namespace Projecten2.Models.Domain
         public int KostenId { get; set; }
         public int AnalyseId { get; set; }
         public Analyse Analyse { get; set; }
+        public ICollection<KVraag01> Kvragen01 { get; set; }
+        public ICollection<KVraag1> Kvragen1 { get; set; }
+        public ICollection<KVraag2> Kvragen2 { get; set; }
+        public ICollection<KVraag3> Kvragen3 { get; set; }
+        public ICollection<KVraag4> Kvragen4 { get; set; }
+        public ICollection<KVraag5> Kvragen5 { get; set; }
+        public ICollection<KVraag6> Kvragen6 { get; set; }
+        public ICollection<KVraag7> Kvragen7 { get; set; }
     }
 }
